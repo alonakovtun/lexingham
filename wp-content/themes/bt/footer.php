b1dcf461c9f725da73b5e8d65b121e7c8b992501
alonakovtun <alonakovtun55@gmail.com>
2022-12-07 11:14:21 +0100

    footer

--- wp-content/themes/bt/footer.php
+++ wp-content/themes/bt/footer.php
@@ -7,7 +7,7 @@ if (!defined('ABSPATH')) {
 ?>
 
 <footer class="main-site-footer site-ex-light-gray-bg coresans-300">
-<div class="find-us-link">
+	<div class="find-us-link">
 		<?php $find_us_link = get_field('find_us_link', 'option');
 		if ($find_us_link) :
 			$link_url = $find_us_link['url'];
@@ -21,57 +21,88 @@ if (!defined('ABSPATH')) {
 	<div class="container-1020 footer-content">
 
 		<?php
-		for ($i = 1; $i < 4; $i++) :
+		for ($i = 1; $i < 3; $i++) :
 			$col_group = get_field('col-' . $i, 'option');
 		?>
 			<div class="col boxed line-height-15">
 				<div class="inner-col position-relative">
-					<div class="content content-no-margin-top-bottom<?= (!empty($col_group['links'])) ? ' padding-bottom-10' : ''; ?>"><?= $col_group['content']; ?></div>
-					<?php if ($i == 3) : ?>
-						<div class="row logos-row">
-							<ul class="logos-list">
-								<?php
-								$links = [
-									[
-										'img' => [
-											'src' => 'https://lexingham.com/wp-content/uploads/2021/12/zzoom-logo.png',
-											'alt' => 'Z-Zoom',
-										],
-										'url' => 'https://z-zoom.co.uk/'
-									],
-									[
-										'img' => [
-											'src' => 'https://lexingham.com/wp-content/uploads/2021/12/travel-blue-logo2.png',
-											'alt' => 'Travel Blue',
-										],
-										'url' => 'https://travel-blue.com/'
-									],
-									[
-										'img' => [
-											'src' => 'https://lexingham.com/wp-content/uploads/2021/12/anomeo-logo.png',
-											'alt' => 'Anomeo',
-										],
-										'url' => 'https://anomeo.com/'
-									],
-								];
-								?>
-								<?php foreach ($links as $link) : ?>
-									<li>
-										<a href="<?= $link['url']; ?>" title="<?= $link['img']['alt']; ?>">
-											<img src="<?= $link['img']['src']; ?>" alt="<?= $link['img']['alt']; ?>">
-										</a>
-									</li>
+					<div class="content content-no-margin-top-bottom"><?= $col_group['content']; ?></div>
+					<?php if ($i == 1) : ?>
+						<?php if (!empty($col_group['social_links'])) : ?>
+
+							<div class="links-wrapper coresans-500 social-links">
+								<?php foreach ($col_group['social_links'] as $social_links) : ?>
+									<?php foreach ($social_links as $social_link) : ?>
+
+
+										<?php
+										$link_url = $social_link['url'];
+										$link_title = $social_link['title'];
+										$link_target = $social_link['target'] ? $social_link['target'] : '_self';
+										// var_dump($link_url);
+
+										?>
+										<a class="link site-blue-link" target="<?php echo esc_attr($link_target); ?>" href="<?php echo esc_url($link_url); ?>"><?php echo esc_html($link_title); ?></a>
+									<?php endforeach; ?>
 								<?php endforeach; ?>
-							</ul>
-						</div>
+
+							</div>
+
+							<div class="links-wrapper coresans-400 links">
+								<?php foreach ($col_group['links'] as $links) : ?>
+									<?php foreach ($links as $link) : ?>
+
+
+										<?php
+										$link_url = $link['url'];
+										$link_title = $link['title'];
+										$link_target = $link['target'] ? $link['target'] : '_self';
+										// var_dump($link_url);
+
+										?>
+										<a class="link site-blue-link" target="<?php echo esc_attr($link_target); ?>" href="<?php echo esc_url($link_url); ?>"><?php echo esc_html($link_title); ?></a>
+									<?php endforeach; ?>
+								<?php endforeach; ?>
+
+							</div>
+
+						<?php endif; ?>
+
 					<?php endif; ?>
-					<?php if (!empty($col_group['links'])) : ?>
-						<div class="links-wrapper coresans-500<?= ($i === 3) ? ' site-blue-link' : ''; ?>">
-							<?php foreach ($col_group['links'] as $link) : ?>
-								<a href="<?= esc_url(urldecode($link['link'])); ?>" title="<?= esc_html($link['link-title']); ?>" target="<?= $link['target']; ?>" class="link site-blue-link"><?= esc_html($link['link-text']); ?></a>
-							<?php endforeach; ?>
-						</div>
+					<?php if ($i == 2) : ?>
+						<?php if (!empty($col_group['group_links'])) : ?>
+							<div class="row logos-row">
+								<ul class="logos-list">
+
+									<?php foreach ($col_group['group_links'] as $group_links) : ?>
+										<?php if ($group_links['image']) : ?>
+
+
+											<?php
+											$link_url = $group_links['link']['url'];
+											$link_title = $group_links['link']['title'];
+											$link_target = $group_links['link']['target'] ? $group_links['link']['target'] : '_self';
+											// var_dump($link_url);
+
+											?>
+											<li>
+												<a class="link site-blue-link" target="<?php echo esc_attr($link_target); ?>" href="<?php echo esc_url($link_url); ?>"><?php echo esc_html($link_title); ?>
+													<img src="<?php echo $group_links['image'] ?>" alt="">
+												</a>
+											</li>
+										<?php endif; ?>
+
+									<?php endforeach; ?>
+								</ul>
+							</div>
+						<?php endif; ?>
+
+
+
+
+
 					<?php endif; ?>
+
 				</div>
 			</div>
 		<?php endfor; ?>
@@ -90,8 +121,6 @@ if (!defined('ABSPATH')) {
 			<span><?= esc_html(get_field('right-copy', 'option')); ?></span><span>&copy;</span>
 		<?php endif; ?>
 	</div>
-
-	<div class="col"></div>
 
 	<div class="col copyright-wrapper-right">
 		<a href="https://parishendzelstudio.com/" class="created-by"><?php _e('by PHS', 'bt') ?></a>
